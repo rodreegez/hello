f6ea683157b3688de8f601fbe36142de05e943e7
Adam <electronicbattleweapon7@gmail.com>
2011-08-09 23:38:10 +0100

    basic generator

---
+++ generators/hello/hello_generator.rb
@@ -0,0 +1,5 @@
+class HelloGenerator < < Rails::Generator::Base
+  def manifest # this method is default entrance of generator
+    puts "hello"
+  end
+end
---
+++ lib/hello.rb
@@ -0,0 +1,5 @@
+require "hello/version"
+
+module Hello
+  # Your code goes here...
+end
